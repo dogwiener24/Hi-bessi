baaa4fe6ea68b167da33d5a363b0d58301fadfb7
dogwiener24 <dogwiener24@gmail.com>
2026-09-05 09:50:00 +0800

    Adjust int_speed values for ANNIHILATING and MIXED modes

--- Mm2.lua
+++ Mm2.lua
@@ -30,9 +30,9 @@ local MODES = {
  PRO          = {h_base=178,h_ping=.22,h_speed=0.02,v_base=170,v_ping=.14,v_dist=.18,sim_base=68,sim_speed=.4,int_base=60,int_speed=-.3,offX=-8,offY=-100,offZ=0},
  INSTINCT     = {h_base=192,h_ping=.30,h_speed=0.02,v_base=180,v_ping=.18,v_dist=.22,sim_base=78,sim_speed=.5,int_base=72,int_speed=-.2,offX=-10,offY=-101,offZ=0},
  SECRETIVE    = {h_base=105,h_ping=.15,h_speed=0.02,v_base=105,v_ping=.10,v_dist=.12,sim_base=28,sim_speed=.2,int_base=60,int_speed=-.4,offX=12,offY=-78,offZ=-1},
- ANNIHILATING = {h_base=185,h_ping=.50,h_speed=0.02,v_base=175,v_ping=.30,v_dist=.35,sim_base=65,sim_speed=1.,int_base=20,int_speed=-.05,offX=-15,offY=-82,offZ=-1},
+ ANNIHILATING = {h_base=185,h_ping=.50,h_speed=0.02,v_base=175,v_ping=.30,v_dist=.35,sim_base=65,sim_speed=1.,int_base=20,int_speed=-.1,offX=-15,offY=-82,offZ=-1},
  ADAPTIVE     = {h_base=125,h_ping=.22,h_speed=0.02,v_base=125,v_ping=.14,v_dist=.18,sim_base=35,sim_speed=.4,int_base=50,int_speed=-.3,offX=-12,offY=-99,offZ=0,auto_switch=true},
- MIXED        = {h_base=218,h_ping=.26,h_speed=0.02,v_base=200,v_ping=.16,v_dist=.20,sim_base=54,sim_speed=.45,int_base=74,int_speed=.05,offX=-12,offY=-197,offZ=-1,auto_switch=true},
+ MIXED        = {h_base=218,h_ping=.26,h_speed=0.02,v_base=200,v_ping=.16,v_dist=.20,sim_base=54,sim_speed=.45,int_base=74,int_speed=-.2,offX=-12,offY=-197,offZ=-1,auto_switch=true},
 }
 local ASUB = {
  CLOSE={h_base=184,h_ping=.28,h_speed=0.002,v_base=173,v_ping=.18,v_dist=.22,sim_base=72,sim_speed=.5,int_base=64,int_speed=-5,offX=-9,offY=-99,offZ=0},
